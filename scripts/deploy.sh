502c0fa9d2bf7e2793202afc455fa84e9756bbb6
Stuart Wakefield <me@stuartwakefield.co.uk>
2016-09-21 08:43:32 +0100

    Using TRAVIS_BRANCH env instead of git command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -ev
-branch="$(git rev-parse --abbrev-ref HEAD)"
-echo "Git branch: $branch"
-if [ "$branch" = "master" ] ; then
-  echo "Deploying..."
+if [ "$TRAVIS_BRANCH" = "master" ] ; then
   bundle exec pod trunk push SwiftDecimalNumber.podspec
 fi
