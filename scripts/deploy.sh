a05329bf321454908136bcd0dc0a9fb37ee1baa6
Stuart Wakefield <me@stuartwakefield.co.uk>
2016-09-21 08:33:13 +0100

    Diagnosing no deployment on Travis CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -ev
-if [ "$(git rev-parse --abbrev-ref HEAD)" = "master" ] ; then
+branch="$(git rev-parse --abbrev-ref HEAD)"
+echo "Git branch: $branch"
+if [ "$branch" = "master" ] ; then
+  echo "Deploying..."
   bundle exec pod trunk push SwiftDecimalNumber.podspec
 fi
